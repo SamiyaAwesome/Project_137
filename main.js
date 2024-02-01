89ef6bc8f6dcee50f70f6cd8815232740ff83174
Samiya Kallu <112356785+SamiyaAwesome@users.noreply.github.com>
2024-01-31 17:06:27 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+objects = [];
+status = "";
+
+
+function setup() {
+  canvas = createCanvas(380, 380);
+  canvas.center();
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+  console.log("Model Loaded!")
+  status = true;
+}
+
+function draw() {
+  image(video, 0, 0, 380, 380);
+}
